d0717345f580506604a9b797b3c68235f7e83d30
michael2893 <terranova.7@osu.edu>
2020-10-19 12:31:15 -0400

    Update ImageClassifier.py

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -68,18 +68,6 @@ first_image = image_batch[0]
 print(np.min(first_image), np.max(first_image)) 
 
 
-
-data_augmentation = keras.Sequential(
-  [
-    layers.experimental.preprocessing.RandomFlip("horizontal", 
-                                                 input_shape=(img_height, 
-                                                              img_width,
-                                                              3)),
-    layers.experimental.preprocessing.RandomRotation(0.1),
-    layers.experimental.preprocessing.RandomZoom(0.1),
-  ]
-)
-
 num_classes = 5
 
 model = Sequential([
@@ -106,6 +94,42 @@ model.compile(optimizer='adam',
 model.summary()
 
 
+epochs = 10
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+data_augmentation = keras.Sequential(
+  [
+    layers.experimental.preprocessing.RandomFlip("horizontal", 
+                                                 input_shape=(img_height, 
+                                                              img_width,
+                                                              3)),
+    layers.experimental.preprocessing.RandomRotation(0.1),
+    layers.experimental.preprocessing.RandomZoom(0.1),
+  ]
+)
+
+model = Sequential([
+  data_augmentation,
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
 epochs = 15
 history = model.fit(
@@ -139,7 +163,7 @@ plt.show()
 
 
 
-path = (''); ### test image
+path = (''); ### Test image
 
 img = keras.preprocessing.image.load_img(
     path, target_size=(img_height, img_width)
